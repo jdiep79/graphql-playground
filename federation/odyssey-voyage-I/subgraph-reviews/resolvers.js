ba844f70839980e3333e51f68bb69e21058f25aa
Jimmy Diep <jdiep79@gmail.com>
2022-05-23 20:31:26 -0700

    extend location entity with reviews

--- federation/odyssey-voyage-I/subgraph-reviews/resolvers.js
+++ federation/odyssey-voyage-I/subgraph-reviews/resolvers.js
@@ -21,6 +21,32 @@ const resolvers = {
       return { id };
     },
   },
+  Location: {
+    /**
+     * This is optional with apollo server
+     * also the data returned here, goes to the parent
+     * for reviewsForLocation and overallRating. If
+     * nothing is defined here, the resolvers below
+     * will get a ref object with the typename and id
+     * only
+     */
+    __resolveReference(ref, _ctx, _info) {
+      console.log('ref', ref);
+      return ref;
+    },
+    reviewsForLocation(parent, _args, { dataSources }) {
+      /**
+       * Parent is the reference object.
+       * Is the id from the argument from
+       * the location query?
+       */
+      console.log('reviews for location', parent);
+      return dataSources.reviewsAPI.getReviewsForLocation(parent.id);
+    },
+    overallRating({ id }, _args, { dataSources }) {
+      return dataSources.reviewsAPI.getOverallRatingForLocation(id);
+    },
+  },
 };
 
 module.exports = resolvers;
